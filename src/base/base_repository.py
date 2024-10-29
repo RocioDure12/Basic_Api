1c5ae00634a4af1df8fbd9582d71488e6bf9a255
Rocio Dure <rocioevelyndure@gmail.com>
2024-10-29 11:56:02 -0300

    abc en baseRepositiry

--- src/base/base_repository.py
+++ src/base/base_repository.py
@@ -1,9 +1,10 @@
 from ..modules.services.db_services import DbServices
 from typing import Generic, TypeVar, List, Optional
+from abc import ABC
 
 T = TypeVar('T')
 
-class BaseRepository(Generic[T]):
+class BaseRepository(ABC, Generic[T]):
 
     
     def __init__(self):
@@ -12,7 +13,7 @@ class BaseRepository(Generic[T]):
     def create(self, item:T)-> T:
         pass
     
-    def read(self):
+    def read(self)-> List[T]:
         pass
     
     def read_by_id(self):
