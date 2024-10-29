c52f7856eab1d849da7b576cdfd400c873e8d6bd
Rocio Dure <rocioevelyndure@gmail.com>
2024-10-29 12:04:06 -0300

    cambios en category

---
+++ src/base/model.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import Field, SQLModel,Column,Date
+from sqlalchemy import BigInteger,Integer
+from abc import ABC, abstractmethod
+
+# primary_key=True,
+
+class BaseModel(SQLModel, table=False):
+    id:Optional[int] = Field(sa_column=Column(BigInteger(), default=None, primary_key=True))
+    deleted_at:Optional[datetime] = None
+    created_at:Optional[datetime] = Field(default_factory=datetime.now,nullable=False)
+    updated_at:Optional[datetime] = None
--- src/modules/models/category.py
+++ src/modules/models/category.py
@@ -2,14 +2,11 @@ from sqlmodel import Field,Relationship, SQLModel
 from typing import Optional
 from datetime import datetime
 from typing import Optional
+from ...base.model import BaseModel
 
-class Category(SQLModel, table=True):
+class Category(BaseModel, table=True):
     __tablename__ ="categories"
-    id:Optional[int]=Field(default=None, primary_key=True)
     category_name:str
-    deleted_at:Optional[datetime] = None
-    created_at:datetime = Field(default_factory=datetime.now,nullable=False)
-    updated_at:Optional[datetime] = None
     user_id:Optional[int]=Field(default=None, foreign_key="user.id")
     
     
