d05feb5a721abbb0f466fa85b3b10e5691be7e6c
Rocio Dure <rocioevelyndure@gmail.com>
2024-11-05 12:46:31 -0300

    cambios en subtasks repository

---
+++ src/modules/repositories/subtasks_repository.py
@@ -0,0 +1,21 @@
+from ..base.repository import BaseRepository
+from ..models.subtask import Subtask
+from typing import List, Type, Optional
+
+class SubtaskRepository(BaseRepository[Subtask]):
+    item:Type[Subtask]=Subtask
+    
+    def __init__(self):
+        super().__init__()
+        
+    def create(self, item:Subtask)->Subtask:
+        return super().create(item)
+    
+    def read_my_subtasks(self, user_id:int)->List[Subtask]:
+        return super().get_items_by_user_id(user_id)
+    
+    def update(self, id, update_item)->Optional[Subtask]:
+        return super().update(id, update_item)
+    
+    def delete(self, id)-> None:
+        return super().delete(id)
