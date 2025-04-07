466e6a3ce57b2518cefe65a8b2c4aad82791810e
Rocio Dure <rocioevelyndure@gmail.com>
2025-04-07 08:11:36 -0300

    ...

--- src/modules/routers/tasks_router.py
+++ src/modules/routers/tasks_router.py
@@ -9,7 +9,7 @@ controller=TasksController()
 
 router.add_api_route('/create',controller.create, methods=['POST'])
 router.add_api_route('/my_tasks',controller.read_my_tasks, methods=['GET'])
-router.add_api_route('/tasks/{id}',controller.get_task_by_id, methods=['GET'])
+router.add_api_route('/task/{id}',controller.get_task_by_id, methods=['GET'])
 router.add_api_route('/{id}',controller.update, methods=['PUT'])
 router.add_api_route('/{id}',controller.delete, methods=['DELETE'])
 
